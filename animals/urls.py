fe121dc255fa43456c6a49f2a3d3ff82ee538b0e
Nicollas Anderson <nicollas_sdias@hotmail.com>
2022-06-24 11:16:52 -0300

    fix: adicionado / na url de animal com id

--- animals/urls.py
+++ animals/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('animals/', views.AnimalView.as_view()),
-    path('animals/<int:animal_id>', views.SingleAnimalView.as_view())
+    path('animals/<int:animal_id>/', views.SingleAnimalView.as_view())
 ]
